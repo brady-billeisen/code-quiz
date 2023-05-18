29f838dfc2328e22cc3e32af3f2e281b9d7c5b7d
brady <bradybill36@gmail.com>
2023-05-18 16:05:03 -0500

    got the new set up done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,62 @@
 var startBtn = document.querySelector('#start-btn');
-var answerBtns = document.querySelectorAll('.answer');
+var titleEl = document.querySelector('#title');
+var questionsEl = document.querySelector('#questions');
 var submitBtn = document.querySelector('#submit');
 var startEl = document.querySelector('#start');
 var quizEl = document.querySelector('#quiz');
 var endEl = document.querySelector('#end');
+var timerEl = document.querySelector('#timer');
 
-function displayQuestion() {}
+var state = {
+    timeRemaining: 90,
+    view: 'START',
+    questionIndex: 0,
+    questions: [],
+}
+
+function setView(view) {
+    startEl.style.display = view === 'START' ? null : 'none';
+    quizEl.style.display = view === 'QUIZ' ? null : 'none';
+    endEl.style.display = view === 'END' ? null : 'none';
+}
+
+function buildQuestion() {
+    var question = state.questions[state.questionIndex];
+    questionsEl.innerHTML = null;
+    for (var choice of question.choices) {
+        var buttonEl = document.createElement('button');
+        buttonEl.textContent = choice;
+        questionsEl.appendChild(buttonEl);
+    }
+}
+
+function init() {
+    timerEl.textContent = state.timeRemaining;
+}
+
+state.questions = [
+    {
+        title: "What should be wrapped around an item/items to signify it's an array?",
+        choices: [
+            "1. {} - Curly brackets",
+            "2. [] - Square brackets",
+            "3. () - Parentheses",
+            "4. // - Double forward slash",
+        ],
+        correct: 1
+    }
+];
 
 
 startBtn.addEventListener('click', function() {
-    startEl.style.display = "none";
-    quizEl.style.display = null;
-    endEl.style.display = "none";
+    setView('QUIZ');
+    buildQuestion();
 });
+questionsEl.addEventListener('click', function() {
+    setView('END');
+})
 submitBtn.addEventListener('click', function() {
-    startEl.style.display = null;
-    quizEl.style.display = "none";
-    endEl.style.display = "none";
+    setView('START');
 });
+
+init();
